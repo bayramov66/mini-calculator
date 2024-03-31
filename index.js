14c21909fc004e4935de48a3c7fd588ff73be4ca
bayramov66 <kamran111.kb@gmail.com>
2024-03-31 00:54:39 -0800

    Mini Calculator ready

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-                   //  1 ci sade usul
-
-// const calcBtn = document.getElementById('calculate')
-// const firstInput = document.getElementById('first')
-// calcBtn.addEventListener('click', () => {
-//     console.log(eval(firstInput.value))
-// })
-
-
-                //  daha genis usul
-
 const firstInput = document.getElementById("first");
 const secondInput = document.getElementById("second");
 const operator = document.getElementById("operator");
@@ -28,30 +17,3 @@ calcBtn.addEventListener("click", () => {
   }
   result.textContent = `${a} ${o} ${b} = ${res}`;
 });
-
-
-// ============ 2 ci usul
-
-// calcBtn.addEventListener("click", () => {
-//       let a = +firstInput.value;
-//       let b = +secondInput.value;
-//       let o = operator.value;
-//       let res;
-//       switch (o) {
-//         case "+":
-//           res = a + b;
-//           break;
-//         case "-":
-//           res = a - b;
-//           break;
-//         case "*":
-//           res = a * b;
-//           break;
-//         case "/":
-//           res = a / b;
-//           break;
-//         default:
-//           res = "Wrong operator...";
-//       }
-//       result.textContent = `${a} ${o} ${b} = ${res}`;
-//     });
