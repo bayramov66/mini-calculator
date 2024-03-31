75b33632d4ccb438305d5a6ea3c193fbdbe351f8
bayramov66 <kamran111.kb@gmail.com>
2024-03-31 01:09:42 -0800

    Mini calculator full ready

--- index.js
+++ index.js
@@ -17,3 +17,93 @@ calcBtn.addEventListener("click", () => {
   }
   result.textContent = `${a} ${o} ${b} = ${res}`;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const firstInput = document.getElementById("first");
+// const secondInput = document.getElementById("second");
+// const operator = document.getElementById("operator");
+// const result = document.getElementById("result");
+// const calcBtn = document.getElementById("calculate");
+// calcBtn.addEventListener("click", () => {
+//   let a = +firstInput.value;
+//   let b = +secondInput.value;
+//   let o = operator.value;
+//   let res = a + b;
+//   if (o === "/") {
+//     res = a / b;
+//   } else if (o === "*") {
+//     res = a * b;
+//   } else if (o === "-") {
+//     res = a - b;
+//   }
+//   result.textContent = `${a} ${o} ${b} = ${res}`;
+// });
+
+
+// const firstInput = document.getElementById("first");
+// const secondInput = document.getElementById("second");
+// const operator = document.getElementById("operator");
+// const result = document.getElementById("result");
+// const calcBtn = document.getElementById("calculate");
+// calcBtn.addEventListener("click", () => {
+//   let a = +firstInput.value;
+//   let b = +secondInput.value;
+//   let o = operator.value;
+//   let res = a + b;
+//   if (o === "/") {
+//     res = a / b;
+//   } else if (o === "*") {
+//     res = a * b;
+//   } else if (o === "-") {
+//     res = a - b;
+//   }
+//   result.textContent = `${a} ${o} ${b} = ${res}`;
+// });
